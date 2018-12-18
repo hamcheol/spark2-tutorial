24079fc4417dab968734bb522d65879a2b5e8907
cheol.ham@navercorp.com <hamcheol@naver.com>
2018-12-18 15:15:09 +0900

    ...

---
+++ spark-highperform-example/src/main/java/com/example/ch03/PandaPlace.java
@@ -0,0 +1,30 @@
+package com.example.ch03;
+
+import java.util.List;
+
+public class PandaPlace {
+	private String name;
+	private List<RawPanda> pandas;
+	
+	public PandaPlace(String name, List<RawPanda> pandas) {
+		this.name = name;
+		this.pandas = pandas;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<RawPanda> getPandas() {
+		return pandas;
+	}
+
+	public void setPandas(List<RawPanda> pandas) {
+		this.pandas = pandas;
+	}
+
+}
